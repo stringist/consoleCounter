94c984f9dca2e3d8941b3029e00865053d240ec7
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-02-09 22:28:57 +0100

    it works!

--- counter.js
+++ counter.js
@@ -1,10 +1,32 @@
 "use strict";
+window.addEventListener("load", count);
 
-let numbers = [0];
-const max = 9;
-// console.log(numbers);
-count();
+let counter = 0;
+const numbers = [0];
 
+function count() {
+    console.log(numbers);
+    counter++;
+    if (numbers.length < 9) {
+        numbers.unshift(counter);
+    } else {
+        numbers.unshift(counter);
+        numbers.length = 9;
+    }
+    setTimeout(count, 1000);
+}
+
+// this one is 90% there, just doesn't keep counting on line 9
+
+// function count() {
+//     console.log(numbers);
+//     counter++;
+//     numbers.unshift(counter);
+//     if (numbers.length <= 9) {
+//         setTimeout(count, 100);
+//     }
+//     // setTimeout(count, 1000);
+// }
 // function count() {
 //     console.log(numbers);
 //     if (numbers.length <= 9) {
@@ -14,12 +36,25 @@ count();
 //         setTimeout(() => { console.log(numbers) }, 1000);
 //     } else count();
 // }
-function count() {
-    // console.log(numbers);
-    numbers.forEach(function(number) { numbers.push(number + 1); })
-    if (numbers.length <= max) {
-        // setTimeout(console.log(numbers), 10000);
-    }
-    console.log(numbers);
-    count();
-}
+// function count() {
+//     // console.log(numbers);
+//     numbers.forEach(function(number) { numbers.push(number + 1); })
+//     if (numbers.length <= max) {
+//         setTimeout(count, 10000);
+//     }
+//     console.log(numbers);
+// }
+
+// function count() {
+
+//     if (numbers.length <= 9) {
+//         numbers.forEach(function(number) {
+//             numbers.push(number++1);
+//             // setTimeout(console.log(numbers), 10000);
+//         });
+//     }
+//     console.log(numbers);
+//     setTimeout(count, 10000);
+//
+
+// }
