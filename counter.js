36b30f75825cd45beb26a0eb9344eb2f9af83779
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-02-09 20:17:35 +0100

    2nd function attempt

---
+++ counter.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let numbers = [0];
+const max = 9;
+
+count();
+
+// function count() {
+//     console.log(numbers);
+//     if (numbers.length <= 9) {
+//         numbers.forEach(function(number) {
+//             numbers.push(number + 1);
+//         });
+//         setTimeout(() => { console.log(numbers) }, 1000);
+//     } else count();
+// }
+function count() {
+
+    numbers.forEach(function(number) { numbers.push(number++); })
+    if (numbers.length <= max) {
+        setTimeout(console.log(numbers), 1000);
+    }
+    console.log(numbers);
+    count();
+}
